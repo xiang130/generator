156cfd966ce2d2687995c130ef05765e7338718b
xiang130 <40787905+xiang130@users.noreply.github.com>
2019-07-31 06:59:08 +0800

    Update FreemarkerUtil.java

--- src/main/java/com/bksuns/mybatis/generator/util/FreemarkerUtil.java
+++ src/main/java/com/bksuns/mybatis/generator/util/FreemarkerUtil.java
@@ -23,12 +23,12 @@ public class FreemarkerUtil {
 		String fileName=dataModel.getEntityName()+messageStr(templateName);
 		try {
 			// 通过FreeMarker的Confuguration读取相应的模板文件
-	        Configuration configuration = new Configuration(Configuration.VERSION_2_3_28);
-	        // 设置模板路径
-	        configuration.setClassForTemplateLoading(FreemarkerUtil.class, "/freemarker/ftl");
-	        // 设置默认字体
-	        configuration.setDefaultEncoding("utf-8");
-	        // 获取模板
+			Configuration configuration = new Configuration(Configuration.VERSION_2_3_28);
+			// 设置模板路径
+			configuration.setClassForTemplateLoading(FreemarkerUtil.class, "/freemarker/ftl");
+			// 设置默认字体
+			configuration.setDefaultEncoding("utf-8");
+			// 获取模板
 			Template template = configuration.getTemplate(templateName);
 			File file = new File(filePath);
 			System.getProperty("file.separator");
@@ -36,33 +36,33 @@ public class FreemarkerUtil {
 				file.getParentFile().mkdirs();
 			}
 			if(!file.exists()) {
-                file.createNewFile();
-            }else {
+				file.createNewFile();
+			}else {
 				file.delete();
 				file.createNewFile();
-            }
-			
+			}
+
 			//设置输出流
-            out = new FileWriter(file);
-            //模板输出静态文件
-            template.process(dataModel, out);
-            result.setCode(1);
-        	result.setMessage("创建"+fileName+"成功");
-        	return result;
+			out = new FileWriter(file);
+			//模板输出静态文件
+			template.process(dataModel, out);
+			result.setCode(1);
+			result.setMessage("创建"+fileName+"成功");
+			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-            if(null != out) {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		    if(null != out) {
+			try {
+			    out.close();
+			} catch (IOException e) {
+			    e.printStackTrace();
+			}
+		    }
+        	}
 		result.setCode(-1);
-    	result.setMessage("创建"+fileName+"失败");
-    	return result;
+		result.setMessage("创建"+fileName+"失败");
+		return result;
 	}
 	
 	public static String messageStr(String name) {
